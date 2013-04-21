b39aecb1493abf1e6d3f9c5f822d7b96332766ba
José Roberto de Souza <zehortigoza@gmail.com>
2013-04-21 20:42:58 -0300

    Initial commit

---
+++ src/org/quadcopter/controller/view/Main.java
@@ -0,0 +1,37 @@
+package org.quadcopter.controller.view;
+import org.quadcopter.controller.R;
+import org.quadcopter.controller.util.VerticalSeekBar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+
+
+public class Main extends Activity implements OnSeekBarChangeListener {
+	private VerticalSeekBar axis_z;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		axis_z = (VerticalSeekBar) findViewById(R.id.seek_bar_axis_z);
+		axis_z.setOnSeekBarChangeListener(this);
+	}
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+		Log.e("Main", "progress="+progress);
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {		
+	}
+
+}
